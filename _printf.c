42411469f8ab7d95b55806e4d38d13b3051aa8fe
Mutali09 <nelsonsimiyu9@gmail.com>
2023-08-20 09:40:11 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,48 +1,65 @@
 #include "main.h"
 #include <stdarg.h>
-void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - prints characters
+ * _printf - printf function
  * @format: is the format.
  * Return: charCount.
  */
 int _printf(const char *format, ...)
 {
+	int charCount = 0;
+	char c;
+	char *str;
 	va_list args;
-	int p, printed = 0, charCount = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	char buffer[BUFF_SIZE];
 
-	if (format == NULL)
-		return (-1);
 	va_start(args, format);
-	for (p = 0; format && format[p] != '\0'; p++)
+	if (format == NULL)
 	{
-		if (format[p] != '%')
+		return (-1);
+	}
+	while (*format)
+	{
+		if (*format == '%')
 		{
-			buffer[buff_ind++] = format[p];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/** write(1, &format[p], 1);*/
+			format++;
+			if (*format == '\0')
+				break;
+			if (*format == 'c')
+			{
+				c = (char)va_arg(args, int);
+				putchar(c);
+				charCount++;
+			}
+			else if (*format == 's')
+			{
+				str = va_arg(args, char *);
+				while (*str)
+				{
+					putchar(*str);
+					str++;
+					charCount++;
+				}
+			}
+			else if (*format == '%')
+			{
+				putchar('%');
+				charCount++;
+			}
+			else
+			{
+				putchar('%');
+				putchar(*format);
+				charCount += 2;
+			}
+		}
+		else
+		{
+			putchar(*format);
 			charCount++;
 		}
-	else
-	{
-		print_buffer(buffer, &buff_ind);
-		flags = get_flags(format, &p);
-		width = get_width(format, &p, args);
-		precision = get_precision(format, &p, args);
-		size = get_size(format, &p);
-		++p;
-		printed = handle_print(format, &p, args, buffer,
-				flags, width, precision, size);
-		if (printed == -1)
-			return (-1);
-		charCount += printed;
+		format++;
 	}
-	}
-	print_buffer(buffer, &buff_ind);
 	va_end(args);
 	return (charCount);
 }
