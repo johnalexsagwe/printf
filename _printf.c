1a19e16c71c44b7d9ff6c7dd30a6abaac92d2855
Mutali09 <nelsonsimiyu9@gmail.com>
2023-08-20 07:41:47 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdarg.h>
-#include <unistd.h>
+void print_buffer(char buffer[], int *buff_ind);
 
 /**
  * _printf - prints characters
@@ -10,36 +10,39 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int charCount = 0;
+	int p, printed = 0, charCount = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
 		return (-1);
 	va_start(args, format);
-	while (*format)
+	for (p = 0; format && format[p] != '\0'; p++)
 	{
-		if (*format == '%')
+		if (format[p] != '%')
 		{
-			format++;
-			if (*format == '\0')
-				break;
-			if (*format == ' ')
-				return (-1);
-			if (*format == 'c')
-				handle_char(args, &charCount);
-			else if (*format == 's')
-				handle_string(args, &charCount);
-			else if (*format == '%')
-				handle_percent(&charCount);
-			else
-				handle_unknown(*format, &charCount);
+			buffer[buff_ind++] = format[p];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/** write(1, &format[p], 1);*/
+			charCount++;
 		}
-		else
-		{
-				write(1, format, 1);
-				charCount++;
-		}
-		format++;
+	else
+	{
+		print_buffer(buffer, &buff_ind);
+		flags = get_flags(format, &p);
+		width = get_width(format, &p, args);
+		precision = get_precision(format, &p, args);
+		size = get_size(format, &p);
+		++p;
+		printed = handle_print(format, &p, args, buffer,
+				flags, width, precision, size);
+		if (printed == -1)
+			return (-1);
+		charCount += printed;
 	}
+	}
+	print_buffer(buffer, &buff_ind);
 	va_end(args);
 	return (charCount);
 }
