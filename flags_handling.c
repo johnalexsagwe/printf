ffc1000172b3a9b9c4dd28efe20fecaa5774597d
johnalexsagwe <johnsagwe@gmail.com>
2023-08-22 01:43:49 -0700

    main.h

--- flags_handling.c
+++ flags_handling.c
@@ -1,16 +1,12 @@
-#include "main.h"
-
 /**
- * get_flags - Extracts active formatting flags
+ * get_flags_and_update - Takes active formatting flags, updates struct flags
  * @format: The formatted string containing the flags
  * @i: Pointer to the current position in the format string
- * 
- * Return: Calculated flags:
+ * @f: Pointer to the struct flags in which we turn the flags on
+ * Return: Calculated flags
  */
-int get_flags(const char *format, int *i)
+int get_flags_and_update(const char *format, int *i, flags_t *f)
 {
-	/* - + 0 # ' ' */
-	/* 1 2 4 8  16 */
 	int j, curr_i;
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
@@ -19,11 +15,14 @@ int get_flags(const char *format, int *i)
 	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 	{
 		for (j = 0; FLAGS_CH[j] != '\0'; j++)
+		{
 			if (format[curr_i] == FLAGS_CH[j])
 			{
 				flags |= FLAGS_ARR[j];
+				get_flag(format[curr_i], f);
 				break;
 			}
+		}
 
 		if (FLAGS_CH[j] == 0)
 			break;
--- number_formatting.c
+++ number_formatting.c
@@ -2,13 +2,14 @@
 
 /************************* PRINT UNSIGNED NUMBER *************************/
 /**
- * print_unsigned - Prints an unsigned number
- * @types: List a of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width
+ * print_unsigned - Output an unsigned number
+ * @types: List of argument
+ * @buffer: Buffer array for handling output
+ * @flags:  Active flags Calculation
+ * @width: Field width
  * @precision: Precision specification
  * @size: Size specifier
+ *
  * Return: Number of chars printed.
  */
 int print_unsigned(va_list types, char buffer[],
